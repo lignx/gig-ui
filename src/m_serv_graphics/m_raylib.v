f355b27322e0dd3b609d176ebf61e159f08fc620
lignx <xzzulz@gmail.com>
2026-03-24 09:06:47 -0400

    Abstract Raylib View functions
    Moved into `m_raylib` module.

--- src/m_serv_graphics/m_raylib.v
+++ src/m_serv_graphics/m_raylib.v
@@ -65,3 +65,30 @@ pub fn get_key_pressed() int {
 pub fn get_char_pressed() int {
 	return raylib.get_char_pressed()
 }
+
+
+// View / Texture functions
+
+pub type RenderTexture2D = raylib.RenderTexture2D
+pub type Rectangle = raylib.Rectangle
+pub type Texture2D = raylib.Texture2D
+pub type Color = raylib.Color
+
+pub const blank = raylib.blank
+pub const white = raylib.white
+
+pub fn begin_texture_mode(target RenderTexture2D) {
+	raylib.begin_texture_mode(target)
+}
+
+pub fn end_texture_mode() {
+	raylib.end_texture_mode()
+}
+
+pub fn clear_background(clr Color) {
+	raylib.clear_background(clr)
+}
+
+pub fn draw_texture_rec(texture Texture2D, source Rectangle, x f32, y f32, clr Color) {
+	raylib.draw_texture_rec(texture, source, raylib.Vector2{x, y}, clr)
+}
--- src/m_view.v
+++ src/m_view.v
@@ -3,14 +3,14 @@
 module m_view
 
 
-import raylib
+import m_serv_graphics.m_raylib
 
 
 
 struct View {
 mut:
-    target     raylib.RenderTexture2D
-    rect       raylib.Rectangle
+    target     m_raylib.RenderTexture2D
+    rect       m_raylib.Rectangle
     is_active  bool
     // z_index for layering
 }
@@ -18,21 +18,21 @@ mut:
 fn (mut v View) begin_draw() {
     
 	// Abstract raylib code @TODO
-	raylib.begin_texture_mode(v.target)
+	m_raylib.begin_texture_mode(v.target)
     
 	// Clear background for this specific view
 	// This is an example operation. Should be in the drawing code.
 	// Not here.
-    raylib.clear_background(raylib.blank) 
+    m_raylib.clear_background(m_raylib.blank) 
 }
 
 fn (mut v View) end_draw() {
-    raylib.end_texture_mode()
+    m_raylib.end_texture_mode()
 }
 
 fn (v View) render_to_screen() {
     // Draw the texture to the main window
     // Note: Raylib textures are Y-flipped, so we'd flip them back here
-    source := raylib.Rectangle{0, 0, v.target.texture.width, -v.target.texture.height}
-    raylib.draw_texture_rec(v.target.texture, source, raylib.Vector2{v.rect.x, v.rect.y}, raylib.white)
+    source := m_raylib.Rectangle{0, 0, v.target.texture.width, -v.target.texture.height}
+    m_raylib.draw_texture_rec(v.target.texture, source, v.rect.x, v.rect.y, m_raylib.white)
 }
